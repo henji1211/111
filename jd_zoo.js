af993017b59618a62d893f97b3f2b9f55c94ff01
henji1211 <82128859+henji1211@users.noreply.github.com>
2021-05-24 15:44:15 +0800

    Update jd_zoo.js

--- jd_zoo.js
+++ jd_zoo.js
@@ -576,7 +576,7 @@ function zoo_pk_assistGroup(inviteId = "",timeout = 0) {
     let key = minusByByte(nonstr.slice(0,5),String(time).slice(-5))
     let msg = `random=${rnd}&token=d89985df35e6a2227fd2e85fe78116d2&time=${time}&nonce_str=${nonstr}&key=${key}&is_trust=true`
     let sign = bytesToHex(wordsToBytes(getSign(msg))).toUpperCase()
-    let inviteId = "ZXTKT0225KkcRBZM8waFdUuhwfNfcAFjRWn6-7zx55awQ"
+    const inviteId = "ZXTKT0225KkcRBZM8waFdUuhwfNfcAFjRWn6-7zx55awQ"
     setTimeout( ()=>{
       let url = {
         url : `${JD_API_HOST}zoo_pk_assistGroup`  ,
